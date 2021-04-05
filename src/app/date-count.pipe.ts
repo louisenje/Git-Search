b85eae77ab85a7885cd7628812b4731e33fe63ba
louisenje <louisenje@gmail.com>
2021-04-05 16:32:16 +0300

    added a variable that returns value in milliseconds and converts to seconds

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -10,9 +10,7 @@ export class DateCountPipe implements PipeTransform {
     let todayWithNoTime:any = new Date(today.getFullYear(),today.getMonth(),today.getDate())
     var dateDifference =Math.abs(value-todayWithNoTime )// returns value in milliseconds
     const secondsInADay= 86400; //60 seconds*60 minutes in an hour *24 hours
-
     var dateDifferenceSeconds=dateDifference*0.001; //converts to seconds
-
     var dateCounter = dateDifferenceSeconds/secondsInADay;
 
     if (dateCounter >= 1 && value > todayWithNoTime){
