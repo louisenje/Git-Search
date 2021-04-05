be06280c26dff2f1f38a865d05f186ce59b6f4ab
louisenje <louisenje@gmail.com>
2021-04-05 15:57:23 +0300

    added the api key in environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey:'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',
+  baseUrl:''
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
